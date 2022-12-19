d6d8007ff66aebb0c5d785d83ff83526b4f22f19
masyhar <masyhar@ilcs.co.id>
2022-12-20 00:24:21 +0700

    day-1-introduction exercise

---
+++ day-1-introduction/datatypes.js
@@ -0,0 +1,22 @@
+let variableA;
+let variableB;
+let variableC;
+let variableD;
+
+let variableE = 'masyhar';
+let variableF = 100;
+let variableG = true;
+let variableH = undefined;
+
+console.log(typeof variableA, typeof variableB, typeof variableC, typeof variableD, typeof variableE, typeof variableF, typeof variableG, typeof variableH);
+
+const firstName = 'Masyhar';
+const lastName = 'Muharam';
+let maritalStatus = false;
+let country = 'Indonesia';
+let age = 24;
+
+const myAge = 24;
+const yourAge = 30;
+console.log('I am ', myAge, 'years old.');
+console.log('You are', yourAge, 'years old.');
---
+++ day-1-introduction/variable.js
@@ -0,0 +1,7 @@
+let username = 'masyhar';
+let isMarried = false;
+let userCar = null;
+let isUndefined = undefined;
+
+console.log(username, isMarried, userCar, isUndefined);
+
