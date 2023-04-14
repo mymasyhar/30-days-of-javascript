0134a975e51576e4fad16a5d2e6d381544485389
mymasyhar <mymasyhar@gmail.com>
2023-04-14 16:50:04 +0700

    day-28-mini-project-leaderboard-[rebuild and add remove function]

--- day-28-mini-project-leaderboard/script.js
+++ day-28-mini-project-leaderboard/script.js
@@ -1,160 +1,108 @@
-const leaderboards = [
+const leaderboardData = [
   {
-    firstName: 'Masyhar',
-    lastName: 'M.',
-    createdAt: '07/04/2023 20:34',
-    country: 'Indonesia',
-    score: 90,
-  },
-  {
-    firstName: 'David',
-    lastName: 'Smith',
-    createdAt: '07/04/2023 20:34',
-    country: 'Canada',
-    score: 90,
-  },
-  {
-    firstName: 'Karim',
-    lastName: 'M.',
-    createdAt: '07/04/2023 20:34',
-    country: 'UK',
-    score: 90,
-  },
+    firstName: 'firstName.value',
+    lastName: 'lastName.value',
+    country: 'country.value',
+    score: 'score.value',
+    createdAt: '14/04/2023 15:53',
+  }
 ];
-const allInputs = document.querySelectorAll('input').forEach(input => {
-  switch(input.id){
-    case 'firstName':
-      input.placeholder = 'first name'.toLocaleUpperCase();
-      break;
-    case 'lastName':
-      input.placeholder = 'last name'.toLocaleUpperCase();
-      break;
-    case 'country':
-      input.placeholder = 'country'.toLocaleUpperCase();
-      break;
-    default:
-      input.placeholder = 'scores'.toLocaleUpperCase();
-      break;
-  }
-  input.style.border = '1px solid #FF3E23';
-  input.style.padding = '10px';
-  input.style.width = '175px';
-});
 
-function headerStyling(){
-  const header = document.querySelector('header');
-  header.style.textAlign = 'center';
+function generateDate(){
+  const date = new Date();
+  const day = date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`;
+  const month = date.getMonth()+1 < 10 ? `0${date.getMonth()+1}` : `${date.getMonth()+1}`;
+  const year = date.getFullYear();
+  const hours = date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`;
+  const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
 
-  const buttonSubmit = document.querySelector('button');
-  buttonSubmit.style.border = 'none';
-  buttonSubmit.style.padding = '12px 20px'
-  buttonSubmit.style.width = '200px'
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
 }
-headerStyling();
+console.log(generateDate());
 
-resultWrapper();
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const country = document.getElementById('country');
+const score = document.getElementById('score');
 
-const buttonSubmit = document.getElementById('submitScore');
-const wrapper = document.querySelector('.wrapper');
-buttonSubmit.addEventListener('click', () => {
-  const fullDate = new Date();
-  const date = fullDate.getDate() < 10 ? `0${fullDate.getDate()}` : `${fullDate.getDate()}`;
-  const month = fullDate.getMonth()+1 < 10 ? `0${fullDate.getMonth()}` : `${fullDate.getMonth()}`;
-  const year = fullDate.getFullYear();
-  const hour = fullDate.getHours() < 10 ? `0${fullDate.getHours()}` : `${fullDate.getHours()}`;
-  const minute = fullDate.getMinutes() < 10 ? `0${fullDate.getMinutes()}` : `${fullDate.getMinutes()}`;
-  const createdAt = `${date}/${month}/${year} ${hour}:${minute}`
-  const firstName = document.getElementById('firstName').value;
-  const lastName = document.getElementById('lastName').value;
-  const country = document.getElementById('country').value;
-  const score = document.getElementById('score').value;
-  
-  const data = {firstName, lastName, createdAt, country, score};
-  if(firstName == null || lastName == null || country == null || score == null){
-    console.log('null data');
-  }else{
-    wrapper.innerHTML = '';
-    leaderboards.push(data);
-    resultWrapper();
+const addLeaderboard = document.getElementById('submitData');
+const container = document.querySelector('.leaderboard-container');
+addLeaderboard.addEventListener('click', () => {
+  const data = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    country: country.value,
+    score: parseInt(score.value),
+    createdAt: generateDate(),
   }
+  leaderboardData.push(data);
+  container.appendChild(renderLeaderboard());
+  renderLeaderboard();
+});
+function renderLeaderboard(){
+  let leaderboardList;
   
-})
-
-function resultWrapper(){
-  const leaderboardsWrapper = document.querySelector('.wrapper');
-  let leaderboardData;
-  
-  let dataName;
-  let name;
+  let nameContainer;
+  let fullName;
   let createdAt;
-  
-  let dataCountry;
+
   let country;
 
-  let dataScore;
   let score;
 
-  let dataOption;
-  let buttonDelete;
-  let buttonDeleteIcon;
-  let buttonPlus;
-  let buttonMinus;
-  
-  for(let i=0; i<leaderboards.length; i++){
-    leaderboardData = document.createElement('div');
+  let actionContainer;
+  let deleteButton;
+  let plusButton;
+  let minusButton;
 
-    dataName = document.createElement('div');
-    name = document.createElement('p');
-    name.textContent = `${leaderboards[i].firstName.toLocaleUpperCase()} ${leaderboards[i].lastName.toLocaleUpperCase()}`;
+  for(let i=0; i<leaderboardData.length; i++){
+    leaderboardList = document.createElement('div');
+    leaderboardList.className = 'leaderboard-list';
+
+    nameContainer = document.createElement('div');
+    fullName = document.createElement('p');
+    fullName.textContent = `${leaderboardData[i].firstName} ${leaderboardData[i].lastName}`;
     createdAt = document.createElement('p');
-    createdAt.textContent = leaderboards[i].createdAt;
+    createdAt.textContent = `${leaderboardData[i].createdAt}`;
+    nameContainer.appendChild(fullName)
+    nameContainer.appendChild(createdAt);
 
-    dataName.appendChild(name);
-    dataName.appendChild(createdAt);
+    leaderboardList.appendChild(nameContainer);
 
-    dataCountry = document.createElement('div');
     country = document.createElement('p');
-    country.textContent = leaderboards[i].country;
+    country.textContent = `${leaderboardData[i].country}`;
 
-    dataCountry.appendChild(country);
-
-    dataScore = document.createElement('div');
+    leaderboardList.appendChild(country);
+    
     score = document.createElement('p');
-    score.textContent = leaderboards[i].score;
+    score.textContent = `${leaderboardData[i].score}`;
 
-    dataScore.appendChild(score);
+    leaderboardList.appendChild(score);
 
-    dataOption = document.createElement('div');
-    buttonDelete = document.createElement('button');
-    buttonDeleteIcon = document.createElement('i');
-    buttonDeleteIcon.className = 'fas fa-trash-can';
-
-    buttonDelete.appendChild(buttonDeleteIcon);
-
-    buttonPlus = document.createElement('button');
-    buttonPlus.textContent = '+5'
+    actionContainer = document.createElement('div');
+    actionContainer.className = 'action-group';
+    deleteButton = document.createElement('button');
+    deleteButton.textContent = 'delete';
+    deleteButton.id = 'delete';
     
-    buttonMinus = document.createElement('button');
-    buttonMinus.textContent = '-5'
+    deleteButton.addEventListener('click', (e) => {
+      e.target.parentNode.parentNode.remove();
+    })
     
-    dataOption.appendChild(buttonDelete)
-    dataOption.appendChild(buttonPlus)
-    dataOption.appendChild(buttonMinus)
+    plusButton = document.createElement('button');
+    plusButton.textContent = 'plus';
+    plusButton.id = 'plus';
+    
+    
+    minusButton = document.createElement('button');
+    minusButton.textContent = 'minus';
+    minusButton.id = 'minus';
+    
+    actionContainer.appendChild(deleteButton)
+    actionContainer.appendChild(plusButton)
+    actionContainer.appendChild(minusButton);
 
-    leaderboardData.appendChild(dataName)
-    leaderboardData.appendChild(dataCountry)
-    leaderboardData.appendChild(dataScore)
-    leaderboardData.appendChild(dataOption);
-
-    leaderboardsWrapper.appendChild(leaderboardData);
-
-    leaderboardData.style.display = 'flex';
-    leaderboardData.style.alignItems = 'center';
-    leaderboardData.style.justifyContent = 'space-between';
-    leaderboardData.style.background = '#FF97ED';
-    leaderboardData.style.marginBottom = '10px';
-    leaderboardData.style.padding = '0 15px';
-
-    leaderboardsWrapper.style.margin = '10px 15%';
+    leaderboardList.appendChild(actionContainer);
   }
+  return leaderboardList;
 }
