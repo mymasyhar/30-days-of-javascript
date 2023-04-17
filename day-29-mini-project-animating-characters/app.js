5f779993218bbe637e09eb7608a5af32b11a5d0e
mymasyhar <mymasyhar@gmail.com>
2023-04-17 10:40:02 +0700

    day-29-mini-project-animating-characters-[initializing]

---
+++ day-29-mini-project-animating-characters/app.js
@@ -0,0 +1,54 @@
+document.body.style.fontFamily = 'Montserrat';
+
+function generateColor(){
+  const colorElement = '1234567890ABCDEF';
+  let colorUsed = `#`;
+  for(let i=0; i<6; i++){
+    colorUsed += colorElement.charAt(Math.floor(Math.random() * colorElement.length));
+  }
+  return colorUsed;
+}
+
+const fontSize = () => {
+  const size = [40,48,52,64,72,80,88,92,100,110];
+  // Math.floor(Math.random() * 100) <= 50 ? `120px` : `${Math.floor(Math.random() * 100)}px`
+  return `${size[Math.floor(Math.random() * size.length)]}px`;
+};
+console.log(fontSize());
+
+const textContainer = document.querySelector("h1");
+function generateStyle(){
+  const text = '30 Days of JavaScript Masyhar Muharam';
+  let insertedChar;
+  for(let i=0; i<text.length; i++){
+    insertedChar = document.createElement('span');
+    insertedChar.textContent = text[i].toLocaleUpperCase();
+    insertedChar.style.color = generateColor();
+    textContainer.appendChild(insertedChar);
+  }
+  textContainer.style.fontSize = fontSize();
+  textContainer.style.position = 'fixed';
+}
+
+const textWrapper = document.querySelector(".text-wrapper");
+const backgroundChange = () =>{
+  textWrapper.style.background = generateColor();
+  textWrapper.style.height = '100vh'
+  textWrapper.style.width = '100%';
+  textWrapper.style.margin = '0';
+  textWrapper.style.position = 'absolute';
+  textWrapper.style.top = '0';
+  textWrapper.style.left = '0';
+  textWrapper.style.padding = '5%';
+}
+
+const animating = () => {
+  textContainer.innerHTML = '';
+  textContainer.style.fontSize = fontSize();
+  generateStyle();
+}
+// animating();
+
+// const result = setInterval(animating, 2000);
+const background = setInterval(backgroundChange, 2000);
+const result = setInterval(animating, 1800);
